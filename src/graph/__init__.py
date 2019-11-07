41850dfaf29b5deb905e043b9d4189c34fe6b676
David Longo <longodj@gmail.com>
2019-11-07 16:47:21 -0500

    skeleton

---
+++ src/graph/__init__.py
@@ -0,0 +1,5 @@
+"""Collect modules for import."""
+
+__author__ = "David Longo (longodj@gmail.com)"
+
+from .mol_features import mol_features  # noqa: F401
---
+++ src/models/modules/gcn.py
@@ -0,0 +1,72 @@
+""" [summary]
+"""
+import argparse
+import numpy as np
+import pytorch as torch
+import torch.nn as nn
+from typing import Tuple
+
+import graph.mol_features as mol_features
+
+__author__ = "David Longo (longodj@gmail.com)"
+
+
+class GraphConvNet(nn.Module):
+    """GraphConvNet [summary].
+
+    Parameters
+    ----------
+    nn : [type]
+        [description]
+    """
+    def __init__(self,
+                 args: argparse.Namespace):
+        super(GraphConvNet, self).__init__()
+        self.args = args
+
+        """Message Passing Network."""
+        self.W_msg_i = nn.Linear(mol_features.N_ATOM_FEATS +
+                                 mol_features.N_BOND_FEATS,
+                                 args.hidden_size,
+                                 bias=False)
+        
+        if args.no_share:
+            self.W_msg_h = nn.ModuleList(
+                [
+                    nn.Linear(args.hidden_size, 
+                              args.hidden_size, 
+                              bias=False)
+                    for _ in range(args.depth - 1)
+                ]
+            )
+        else:
+            self.W_msg_h = nn.Linear(args.hidden_size, 
+                                     args.hidden_size, 
+                                     bias=False)
+        
+        self.W_msg_o = nn.Linear(mol_features.N_ATOM_FEATS + args.hidden_size,
+                                 args.hidden_size)
+        
+        """Dropout."""
+        self.dropout = nn.Dropout(args.dropout)
+
+    """ [summary]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    def forward(self, 
+                graph_inputs: Tuple[torch.Tensor,   # fatoms
+                                    torch.Tensor,   # fbonds
+                                    torch.Tensor,   # agraph
+                                    torch.Tensor]   # bgraph
+                ) -> torch.Tensor:
+        fatoms, fbonds, agraph, bgraph = graph_inputs
+        
+        atom_input = torch.tensor(None)
+        atom_input = self.dropout(atom_input)
+        
+        atom_h = nn.ReLU()(self.W_message_o(atom_input))
+        return atom_h
